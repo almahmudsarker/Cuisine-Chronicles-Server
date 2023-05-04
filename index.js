833e5437004fff1eb4999a84be11161509c11159
almahmudsarker <almahmuds427@gmail.com>
2023-05-04 21:19:33 +0600

    server update

--- index.js
+++ index.js
@@ -16,14 +16,14 @@ app.get("/chefs", (req, res) => {
   res.send(chefs);
 });
 
-// //specific chefs
+// //each  chefs
 app.get("/chefs/:id", (req, res) => {
-  const id = req.params.id;
+  const id = parseInt(req.params.id);
   if (id === 0) {
-    res.send(chefs);
+    res.send(recipe);
   } else {
-    const specificChefs = chefs.filter((n) => n.id === id);
-    res.send(specificChefs);
+    const categoryChefs = recipe.filter((n) => parseInt(n.category_id) === id);
+    res.send(categoryChefs);
   }
 });
 
@@ -37,20 +37,10 @@ app.get("/recipe", (req, res) => {
 // //specific recipe
 app.get("/recipe/:id", (req, res) => {
   const id = req.params.id;
-  const selectedRecipe = recipe.find((r) => r.id === id);
+  const selectedRecipe = recipe.find((r) => r._id === id);
   res.send(selectedRecipe);
 });
 
-// //category wise news
-// app.get("/categories/:id", (req, res) => {
-//   const id = parseInt(req.params.id);
-//   if (id === 0) {
-//     res.send(news);
-//   } else {
-//     const categoryNews = news.filter((n) => parseInt(n.category_id) === id);
-//     res.send(categoryNews);
-//   }
-// });
 
 app.listen(port, () => {
   console.log(`Assignment-10-server listening on port ${port}`);
